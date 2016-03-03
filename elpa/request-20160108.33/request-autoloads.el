6298b1dcbbc088f277164de686d2f0a412e9dc66
hongziki <hongziki@hhh.com>
2016-03-03 17:58:01 +0800

    m

---
+++ elpa/request-20160108.33/request-autoloads.el
@@ -0,0 +1,15 @@
+;;; request-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("request.el") (22232 736 650236 405000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; request-autoloads.el ends here
