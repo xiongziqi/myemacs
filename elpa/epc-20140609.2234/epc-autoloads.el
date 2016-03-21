e0c4ebfc7d890e3a9f9ffcfd7b41dbd7f3bd61aa
hongziki <hongziki@hhh.com>
2016-03-21 10:29:20 +0800

    m

---
+++ elpa/epc-20140609.2234/epc-autoloads.el
@@ -0,0 +1,16 @@
+;;; epc-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("epc-pkg.el" "epc.el" "epcs.el") (22237
+;;;;;;  18307 214908 241000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; epc-autoloads.el ends here
---
+++ elpa/rtags-20160306.1546/rtags-pkg.el
@@ -0,0 +1,4 @@
+(define-package "rtags" "20160306.1546" "A front-end for rtags" 'nil :url "http://rtags.net")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
