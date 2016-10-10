acaa0233088184a00dbb86dfc90848c29887c76c
XiongZiQi <xiong>
2016-10-10 14:35:30 +0800

    m

--- elpa/yasnippet-20160104.129/snippets/python-mode/.yas-setup.el
+++ elpa/yasnippet-20160104.129/snippets/python-mode/.yas-setup.el
@@ -11,12 +11,12 @@
          (max-len (if args (apply 'max (mapcar (lambda (x) (length (nth 0 x))) args)) 0))
          (formatted-args (mapconcat
                 (lambda (x)
-                   (concat (nth 0 x) (make-string (- max-len (length (nth 0 x))) ? ) " -- "
+                   (concat "    " (nth 0 x) (make-string (- max-len (length (nth 0 x))) ? ) ": "
                            (if (nth 1 x) (concat "\(default " (nth 1 x) "\)"))))
                 args
                 indent)))
     (unless (string= formatted-args "")
-      (mapconcat 'identity (list "Keyword Arguments:" formatted-args) indent))))
+      (mapconcat 'identity (list "Args:" formatted-args) indent))))
 
 (add-hook 'python-mode-hook
           '(lambda () (set (make-local-variable 'yas-indent-line) 'fixed)))
